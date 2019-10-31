bcf5b6b9b13199f9b9a453c2f561f8a0d6171af6
Aaron Jacobson <aaronlj@uw.edu>
2019-10-30 19:27:27 -0700

    Added fixed assets from after demo

---
+++ Fixed Assets/Assets/Demo/EnemyFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using Assets;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+
+    public EnemySO enemySo;
+
+    private Quaternion originalOrientation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.movementSpeed = enemySo.speed;
+        //this.transform.localScale = new Vector3(enemySo.size, enemySo.size, 1);
+
+        this.originalOrientation = transform.rotation;
+        GetComponent<SpriteRenderer>().sprite = this.enemySo.image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        transform.rotation = this.originalOrientation;
+    }
+}
---
+++ Fixed Assets/Assets/Demo/EnemySO.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+namespace Assets
+{
+    [CreateAssetMenu(fileName = "enemy", menuName = "Enemy/FloatingEnemy")]
+    public class EnemySO : ScriptableObject
+    {
+        public Sprite image;
+        public float speed;
+
+        public int health;
+        public float size;
+    }
+
+    [CreateAssetMenu(fileName ="t2",menuName ="Friend")]
+    public class FriendSO : ScriptableObject
+    {
+        public int health;
+    }
+}
