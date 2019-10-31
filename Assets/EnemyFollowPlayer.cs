1754ff14d1dc0396ee33a237534e3a115bbf4659
Aaron Jacobson <aaronlj@uw.edu>
2019-10-30 17:47:48 -0700

    Added Predome assets

---
+++ Assets/EnemyFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Assets;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+
+    public EnemySO enemySo;
+
+    private Quaternion originalOrientation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originalOrientation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        transform.rotation = this.originalOrientation;
+    }
+}
---
+++ Assets/EnemySO.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+namespace Assets
+{
+    [CreateAssetMenu(fileName = "enemy", menuName = "Enemy/FloatingEnemy")]
+    public class EnemySO : ScriptableObject
+    {
+        public Sprite image;
+        public float speed;
+    }
+}
